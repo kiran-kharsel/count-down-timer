dcdb69ec0dd48f51b68b6edc3930ac7e01a918bd
kiran-kharsel <kirankharsel@gmail.com>
2025-12-10 07:09:51 +0530

    add show time functionality

--- main.js
+++ main.js
@@ -1,40 +1,56 @@
-
 // dom elem
-const daysElem = document.querySelector('.timer .days')
-const hoursElem = document.querySelector('.timer .hours')
-const minsElem = document.querySelector('.timer .mins')
-const secElem = document.querySelector('.timer .seconds')
+const daysElem = document.querySelector(".timer .days");
+const hoursElem = document.querySelector(".timer .hours");
+const minsElem = document.querySelector(".timer .mins");
+const secElem = document.querySelector(".timer .seconds");
+const startBtn = document.querySelector(".input button");
 
 // count interval
 let countdownInterval;
 
-function srartCountDown(){
-  clearInterval(countdownInterval)
+startBtn.addEventListener("click", function () {
+  startCountdown();
+});
 
-  const targetDate = new Date('2026-01-01').getTime();
-  
+function startCountdown() {
+  clearInterval(countdownInterval);
+
+  const dateInput = document.getElementById("dateInput").value;
+  if (!dateInput) {
+    alert("Please select a date and time!");
+    return;
+  }
+
+  const targetDate = new Date(dateInput).getTime();
+
   countdownInterval = setInterval(() => {
     const now = new Date().getTime();
     const remainingTime = targetDate - now;
 
-    if(remainingTime <= 0){
+    if (remainingTime <= 0) {
       clearInterval(countdownInterval);
+      showTime('00', '00', '00', '00')
       return;
-    };
+    }
 
-    const days = Math.floor(remainingTime / (1000 * 60 * 60* 24))
-    const hours = Math.floor((remainingTime % (1000 * 60 * 60* 24)) / (1000 * 60 * 60))
-    const minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60))
+    const days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor(
+      (remainingTime % (1000 * 60 * 60)) / (1000 * 60)
+    );
     const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
 
-
-    daysElem.innerHTML = days;
-    hoursElem.innerHTML = hours;
-    minsElem.innerHTML = minutes;
-    secElem.innerHTML = seconds;
+    showTime(days, hours, minutes, seconds);
 
 
   }, 1000);
-};
+}
 
-srartCountDown()
+function showTime(days, hours, minutes, seconds) {
+  daysElem.innerHTML = days;
+  hoursElem.innerHTML = hours;
+  minsElem.innerHTML = minutes;
+  secElem.innerHTML = seconds;
+}
